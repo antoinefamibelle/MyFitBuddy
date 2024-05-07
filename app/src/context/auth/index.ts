29099cc36fd539c78660219e011ba9f6ca3e5535
Antoine Famibelle <antoine@endless.fr>
2024-05-07 17:17:40 +0200

    [FIX] The linter is now compiling with warning

--- app/src/context/auth/index.ts
+++ app/src/context/auth/index.ts
@@ -3,5 +3,6 @@ import { createContext } from "react";
 
 export const AuthContext = createContext({
   user: null as UserAuthRo | null,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   setUser: (user: UserAuthRo | null) => {},
 });
--- app/src/context/loading/index.ts
+++ app/src/context/loading/index.ts
@@ -2,5 +2,6 @@ import { createContext } from "react";
 
 export const LoadingContext = createContext({
     loading: false,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
     setLoading: (loading: boolean) => {},
 });
